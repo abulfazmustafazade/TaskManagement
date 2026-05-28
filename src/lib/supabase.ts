388d9238b60c64b50d1619408f87bfb0377b5491
Abulfaz Mustafazade <74773667+abulfazmustafazade@users.noreply.github.com>
2026-05-28 06:32:03 +0400

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from '@/types';
 
-const SUPABASE_URL = (import.meta as any).env?.VITE_SUPABASE_URL || 'https://kmsydkynixmgozxoszie.supabase.co';
-const SUPABASE_ANON_KEY = (import.meta as any).env?.VITE_SUPABASE_ANON_KEY || 'sb_publishable_r0f2nkUiwyHiST6Xpoiu2A_Ek3_duAC';
+const SUPABASE_URL = (import.meta as any).env?.'https://kmsydkynixmgozxoszie.supabase.co';
+const SUPABASE_ANON_KEY = (import.meta as any).env?.'sb_publishable_r0f2nkUiwyHiST6Xpoiu2A_Ek3_duAC';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
